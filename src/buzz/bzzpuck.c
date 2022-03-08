50305dc6c42b198c4975ea793b12c5e765311e36
AidenNeale <aidenneale@sky.com>
2022-03-08 17:44:12 +0000

    Removed redundant code

--- src/buzz/bzzpuck.c
+++ src/buzz/bzzpuck.c
@@ -32,89 +32,77 @@ static void ctrlc_handler(int sig) {
 }
 
 int main(int argc, char** argv) {
-   /* Parse command line */
-   if(argc < 5) usage(argv[0], 0);
-   /* The stream type */
-   char* stream = argv[1];
-   if(strcmp(stream, "tcp") != 0 &&
-      strcmp(stream, "bt") != 0) {
-      fprintf(stderr, "%s: unknown stream type '%s'\n", argv[0], stream);
-      usage(argv[0], 0);
+  /* Parse command line */
+  if(argc < 5) usage(argv[0], 0);
+
+  /* The stream type */
+  char* stream = argv[1];
+  if(strcmp(stream, "tcp") != 0 &&
+    strcmp(stream, "bt") != 0) {
+    fprintf(stderr, "%s: unknown stream type '%s'\n", argv[0], stream);
+    usage(argv[0], 0);
+  }
+
+  /* The message size for robot message sending */
+  char* endptr;
+  int msg_sz = strtol(argv[2], &endptr, 10);
+
+  if(endptr == argv[2] || *endptr != '\0') {
+    fprintf(stderr, "%s: can't parse '%s' into a number\n", argv[0], argv[2]);
+    return 0;
+  }
+  if(msg_sz <= 0) {
+    fprintf(stderr, "%s: invalid value %d for message size\n", argv[0], msg_sz);
+    return 0;
+  }
+
+  // ROBOT ID
+  int RID = 0;
+  if(argc >= 6)
+    RID = strtol(argv[5], &endptr, 10);
+
+  // SERVER
+  SERVER_ADDR = "127.0.0.1"; // Default Server Address
+  // SERVER_ADDR = "192.168.2.101";
+  if(argc >= 7)
+    SERVER_ADDR = argv[6];
+
+  // frequency
+  FREQUENCY = 100000;
+  if(argc == 8)
+    FREQUENCY = 1000000 / strtol(argv[7], &endptr, 10);
+
+  /* The bytecode filename */
+  char* bcfname = argv[3];
+  /* The debugging information file name */
+  char* dbgfname = argv[4];
+
+  /* Wait for connection */
+  /* this function invokes the buzz_listen_tcp() in case
+  of using tcp stream, which creates a thread that runs the
+  function called buzz_stream_incoming_thread_tcp()*/
+  if(!buzz_listen(stream, msg_sz)) return 1;
+
+  /* Set CTRL-C handler */
+  signal(SIGTERM, ctrlc_handler);
+  signal(SIGINT, ctrlc_handler);
+
+  /* Set the Buzz bytecode */
+  if(buzz_script_set(bcfname, dbgfname, RID)) {
+    static struct timeval t1, t2;
+    /* Main loop */
+    while(!done && !buzz_script_done()) {
+      gettimeofday(&t1, NULL);
+      buzz_script_step();
+      gettimeofday(&t2, NULL);
+      double elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000000 + (t2.tv_usec - t1.tv_usec);
+      double sleepTime = FREQUENCY - elapsedTime;
+      if(sleepTime>0){
+        usleep(sleepTime);
+      }
+    /* Cleanup */
+    buzz_script_destroy();
    }
-   /* The message size for robot message sending */
-   char* endptr;
-   int msg_sz = strtol(argv[2], &endptr, 10);
-   if(endptr == argv[2] || *endptr != '\0') {
-      fprintf(stderr, "%s: can't parse '%s' into a number\n", argv[0], argv[2]);
-      return 0;
-   }
-   if(msg_sz <= 0) {
-      fprintf(stderr, "%s: invalid value %d for message size\n", argv[0], msg_sz);
-      return 0;
-   }
-
-   // ROBOT ID
-   int RID = 0;
-   if(argc >= 6)
-     RID = strtol(argv[5], &endptr, 10);
-
-   // SERVER
-   SERVER_ADDR = "127.0.0.1";
-   // SERVER_ADDR = "192.168.2.101";
-   if(argc >= 7)
-     SERVER_ADDR = argv[6];
-
-   // frequency
-   FREQUENCY = 100000;
-   if(argc == 8)
-     FREQUENCY = 1000000 / strtol(argv[7], &endptr, 10);
-   /* The bytecode filename */
-   char* bcfname = argv[3];
-   /* The debugging information file name */
-   char* dbgfname = argv[4];
-
-   /*start FC (flight controller) thread*/
-   //fc_inav_main();
-
-   /* Wait for connection */
-   /* this function invokes the buzz_listen_tcp() in case
-   of using tcp stream, which creates a thread that runs the
-   function called buzz_stream_incoming_thread_tcp()*/
-   if(!buzz_listen(stream, msg_sz)) return 1;
-
-   /* Set CTRL-C handler */
-   signal(SIGTERM, ctrlc_handler);
-   signal(SIGINT, ctrlc_handler);
-   /* Initialize the robot */
-   pipuck_setup();
-   /*initialize camera thread*/
-   //camera_routine();
-   /*initialize LED thread*/
-   // start_blink();
-
-   /* Set the Buzz bytecode */
-   if(buzz_script_set(bcfname, dbgfname,RID)) {
-      static struct timeval t1, t2;
-      /* Main loop */
-      while(!done && !buzz_script_done()) {
-        gettimeofday(&t1, NULL);
-        buzz_script_step();
-        gettimeofday(&t2, NULL);
-        double elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000000 + (t2.tv_usec - t1.tv_usec);
-        double sleepTime = FREQUENCY-elapsedTime;
-        // fprintf(stderr,"[Warning] Program running slower than %f Hz : elapsed time : %.4fuS.\n",1000000/FREQUENCY,elapsedTime);
-        if(sleepTime<0){
-          // fprintf(stderr,"[Warning] Program running slower than %f Hz : elapsed time : %.4fuS.\n",1000000/FREQUENCY,elapsedTime);
-         }
-         else
-            usleep(sleepTime);
-         }
-      /* Cleanup */
-      buzz_script_destroy();
-   }
-
-   /* Stop the robot */
-   pipuck_done();
    /* All done */
    return 0;
 }
