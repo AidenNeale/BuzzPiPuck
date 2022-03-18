44c9749da783887ebdc2c21887680d5b7301bb88
AidenNeale <aidenneale@sky.com>
2022-03-18 14:35:19 +0000

    Removed unused variables

--- src/buzz/pipuck_utility.c
+++ src/buzz/pipuck_utility.c
@@ -7,15 +7,6 @@
 /****************************************/
 /****************************************/
 
-static const int ACC_INC       = 3;
-static const int ACC_DIV       = 0;
-static const int MIN_SPEED_ACC = 20;
-static const int MIN_SPEED_DEC = 1;
-static const int MAX_SPEED     = 400; /* mm/sec */
-
-/****************************************/
-/****************************************/
-
 void pipuck_setup() {
   fprintf(stdout, "Robot setup.\n");
 }
